1557c767f6940b466b93adb9d2d49eec965c5699
Aidan Zimmer <aidan.zimmer@sap.com>
2022-11-21 15:48:22 +0100

    feat: established route and first styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import OverviewView from './views/OverviewView';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div> 
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">OverviewView</Link>
+          </li>
+          <li>
+            <Link to="/header">Header</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>     
+        <Route path="/" element={<OverviewView />} />     
+        <Route path="/header" element={<Header />} />
+      </Routes>
     </div>
+  </BrowserRouter>      
   );
 }
 
