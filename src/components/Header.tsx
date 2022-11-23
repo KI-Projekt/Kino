25803f8f0a631fdbe63832561f49b19a9c052fe7
I549511 <johanna.deike@sap.com>
2022-11-23 15:26:20 +0100

    feat: login first steps

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,6 +24,7 @@ import SearchIcon from '@mui/icons-material/Search';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import MailIcon from '@mui/icons-material/Mail';
 import NotificationsIcon from '@mui/icons-material/Notifications';
@@ -33,6 +34,8 @@ import MoreIcon from '@mui/icons-material/MoreVert';
 import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Modal, Popover, TextField } from '@mui/material';
 import Login from './Login';
 >>>>>>> 5b2b057 (feat: header first steps login)
+=======
+>>>>>>> ed17831 (feat: login first steps)
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
@@ -212,32 +215,61 @@ function Header() {
 
 function Header() {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
-    React.useState<null | HTMLElement>(null);
 
   const isMenuOpen = Boolean(anchorEl);
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
   const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   };
 
-  const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null);
-  };
-
   const handleMenuClose = () => {
     setAnchorEl(null);
-    handleMobileMenuClose();
   };
-
-  const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
-
+  
   const menuId = 'primary-search-account-menu';
+  const renderMenu = ( 
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>LogIn</MenuItem>
+      
+    </Menu>
+  ); 
 
-  const mobileMenuId = 'primary-search-account-menu-mobile';
+
+  // if logged in
+  /* ( 
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  ); */
 
   const open = Boolean(anchorEl);
   const id = open ? 'simple-popover' : undefined;
@@ -256,29 +288,6 @@ function Header() {
           >
             <MenuIcon />
           </IconButton>
-          <Dialog open={open} onClose={handleMenuClose}>
-            <Login></Login>
-            {/* <DialogTitle>Subscribe</DialogTitle>
-            <DialogContent>
-              <DialogContentText>
-                To subscribe to this website, please enter your email address here. We
-                will send updates occasionally.
-              </DialogContentText>
-              <TextField
-                autoFocus
-                margin="dense"
-                id="name"
-                label="Email Address"
-                type="email"
-                fullWidth
-                variant="standard"
-              />
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={handleClose}>Cancel</Button>
-              <Button onClick={handleClose}>Subscribe</Button>
-            </DialogActions> */}
-          </Dialog>
           <Typography
             variant="h6"
             noWrap
@@ -310,20 +319,9 @@ function Header() {
               <AccountCircle />
             </IconButton>
           </Box>
-          {/* <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="show more"
-              aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
-              color="inherit"
-            >
-              <MoreIcon />
-            </IconButton>
-          </Box> */}
         </Toolbar>
       </AppBar>
+      {renderMenu}
     </Box>
   );
 >>>>>>> a3a3d54 (feat: header first setup)
