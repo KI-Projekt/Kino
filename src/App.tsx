c19bc9202ddcc4af7fb3a9c097dab7845be32c47
I549511 <johanna.deike@sap.com>
2022-11-23 17:58:08 +0100

    feat: Menu PopOut first steps

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Header from './components/Header';
 import OverviewView from './views/OverviewView';
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,27 @@
 import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
+import { styled, alpha, useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
-import Badge from '@mui/material/Badge';
-import MenuItem from '@mui/material/MenuItem';
+import PaidIcon from '@mui/icons-material/Paid';
+import SlideshowIcon from '@mui/icons-material/Slideshow';
+import MovieIcon from '@mui/icons-material/Movie';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import SearchIcon from '@mui/icons-material/Search';
 import AccountCircle from '@mui/icons-material/AccountCircle';
-import MailIcon from '@mui/icons-material/Mail';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import MoreIcon from '@mui/icons-material/MoreVert';
-import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Modal, Popover, TextField } from '@mui/material';
+import { Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import Login from './Login';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
@@ -58,85 +63,155 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
-function Header() {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+const drawerWidth = 240;
 
-  const isMenuOpen = Boolean(anchorEl);
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
+function Header() {
+ // const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [anchorElProfile, setAnchorElProfile] = React.useState<null | HTMLElement>(null);
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+
+  const isProfileMenuOpen = Boolean(anchorElProfile);
+  //const isMenuOpen = Boolean(anchorEl);
 
   const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
+    setAnchorElProfile(event.currentTarget);
+  };
+
+  const handleMenuOpen =() => {
+    setOpen(true);
   };
 
   const handleMenuClose = () => {
-    setAnchorEl(null);
+    setOpen(false);
   };
-  
+
+  const handleProfileMenuClose = () => {
+    setAnchorElProfile(null);
+  };
+
+
   const menuId = 'primary-search-account-menu';
-  const renderMenu = ( 
+  const profileMenuId = 'primary-search-account-profile-menu';
+  const renderProfile = (
     <Menu
-      anchorEl={anchorEl}
+      anchorEl={anchorElProfile}
       anchorOrigin={{
         vertical: 'top',
         horizontal: 'right',
       }}
-      id={menuId}
+      id={profileMenuId}
       keepMounted
       transformOrigin={{
         vertical: 'top',
         horizontal: 'right',
       }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
+      open={isProfileMenuOpen}
+      onClose={handleProfileMenuClose}
     >
-      <MenuItem onClick={handleMenuClose}>LogIn</MenuItem>
-      
+      <Box
+        sx={{
+          width: '20rem',
+          height: '20rem',
+        }}>
+        <Login></Login>
+      </Box>
     </Menu>
-  ); 
+  );
 
-
-  // if logged in
-  /* ( 
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-    </Menu>
-  ); */
-
-  const open = Boolean(anchorEl);
-  const id = open ? 'simple-popover' : undefined;
+ const renderMenu = (
+  <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleMenuClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {['Movies', 'Shows'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <MovieIcon /> : <SlideshowIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['Opening hours', 'Ticket prices'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <AccessTimeIcon /> : <PaidIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+ );
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" open={open}>
         <Toolbar>
           <IconButton
             size="large"
             edge="start"
             color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
+            aria-label="open menu"
+            aria-controls={menuId}
+            aria-haspopup="true"
+            onClick={handleMenuOpen}
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
           >
             <MenuIcon />
           </IconButton>
           <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ display: { xs: 'none', sm: 'block' } }}
+             variant="h6" noWrap component="div"
           >
             Cinetastisch
           </Typography>
@@ -155,7 +230,7 @@ function Header() {
               size="large"
               edge="end"
               aria-label="account of current user"
-              aria-controls={menuId}
+              aria-controls={profileMenuId}
               aria-haspopup="true"
               onClick={handleProfileMenuOpen}
               color="inherit"
@@ -164,7 +239,8 @@ function Header() {
             </IconButton>
           </Box>
         </Toolbar>
-      </AppBar>
+      </AppBar>      
+      {renderProfile}
       {renderMenu}
     </Box>
   );
