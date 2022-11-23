e01647a74eb85fbd08cd050056e6bf9845903c15
I549511 <johanna.deike@sap.com>
2022-11-23 11:18:10 +0100

    Revert "Merge pull request #2 from mabu2807/feat/TGB-39/createLogin"
    This reverts commit dba0bb03c8c71f7c8e0efebd01c12a6989f5cb98, reversing
    changes made to f96baff77c7608b12a4a48d2755e3c756c5f971e.

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,97 +1,11 @@
-import { Box, Button, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField } from "@mui/material";
-import React from "react"
-import '../styles/Login.css';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-
-interface State {
-  amount: string;
-  password: string;
-  weight: string;
-  weightRange: string;
-  showPassword: boolean;
-}
+import * as React from 'react';
 
 function Login() {
-  const [values, setValues] = React.useState<State>({
-    amount: '',
-    password: '',
-    weight: '',
-    weightRange: '',
-    showPassword: false,
-  });
+    return ( 
+        <div>
 
-  const handleChange =
-    (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
-      setValues({ ...values, [prop]: event.target.value });
-    };
-
-  const handleClickShowPassword = () => {
-    setValues({
-      ...values,
-      showPassword: !values.showPassword,
-    });
-  };
-
-  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-  };
-
-  return (
-    <div className="Login-form-container">
-      <div className="Login-form">
-        <div className="Login-form-content">
-          <h3 className="Login-form-title">Sign In</h3>
-          <div className="text-center">
-            Not registered yet? <a href="signUp">Sign Up</a>
-          </div>
-          <Box
-            component="form"
-            sx={{
-              '& .MuiTextField-root': { m: 0.5, width: '100%' },
-            }}
-            noValidate
-            autoComplete="off"
-          >
-            <TextField
-              type="email"
-              className="form-login-input"
-              placeholder="Jane.doe@example.com"
-              label="Email Address"
-            />
-            <FormControl sx={{ m: 0.5, width: '100%' }} variant="outlined">
-              <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-              <OutlinedInput
-                id="outlined-adornment-password"
-                type={values.showPassword ? 'text' : 'password'}
-                value={values.password}
-                onChange={handleChange('password')}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
-                    >
-                      {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-                label="Password"
-              />
-            </FormControl>
-            <Button
-              sx={{ m: 0.5, width: '100%' }}
-              variant="contained"
-            >
-              Sign In
-            </Button>
-          </Box>
         </div>
-      </div>
-    </div>
-  )
+     );
 }
 
 export default Login;
