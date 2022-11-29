d3d70988e7a23f117fe88e76eebd8a5f7258b62d
Lisa Ress-Park <s212547@student.dhbw-mannheim.de>
2022-11-29 13:16:37 +0100

    refactor: changes according to review

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import Login from './components/Login';
-import Footer from './components/Footer';
-import OverviewView from './views/OverviewView';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Footer from "./components/Footer";
+import OverviewView from "./views/OverviewView";
 
 function App() {
   return (
-      <BrowserRouter>
-        <div> 
-          <Routes>     
-            <Route path="/" element={<OverviewView />} />     
-          </Routes>
-          <Footer />
-        </div>
-
-      </BrowserRouter>      
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<OverviewView />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,53 +1,97 @@
-import * as React from 'react';
+import * as React from "react";
 import "../styles/Footer.css";
-import { Container, Grid, Box, Link, Typography }from '@mui/material/';
-import InfoIcon from '@mui/icons-material/Info';
-import AssistantDirectionIcon from '@mui/icons-material/AssistantDirection';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { Container, Grid, Box, Link, Typography } from "@mui/material/";
+import InfoIcon from "@mui/icons-material/Info";
+import AssistantDirectionIcon from "@mui/icons-material/AssistantDirection";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
-export default function Footer(){
-    return <div>
-        <Box className="footerBox">
-            <Container className="footerContainer" sx={{width:'100%'}}>
-                <Grid container spacing={0} direction="row"  justifyContent="center"  alignItems="center">
-                    <Grid item xs={4} sm={4}>
-                    </Grid>
-                    <Grid  item xs={4} sm={4}>
-                        <Box>
-                            <Typography align='center'> <InfoIcon className='FooterIcons' ></InfoIcon> <AssistantDirectionIcon className='FooterIcons'></AssistantDirectionIcon><AccountCircleIcon className='FooterIcons'></AccountCircleIcon></Typography>
-                            <Typography align='center'>
-                                <Link className="FooterLink" href="/" color="inherit" underline="none" variant="subtitle2">
-                                    Info ᛫
-                                </Link>
-                                <Link className="FooterLink" href="/" color="inherit" underline="none" variant="subtitle2">
-                                    Support ᛫
-                                </Link>
-                                <Link className="FooterLink" href="/" color="inherit" underline="none" variant="subtitle2">
-                                    Marketing
-                                </Link>
-                            </Typography>
-                        </Box>
-                        
-                        <Box>
-                            <Typography align='center'>
-                            <Link className="FooterLink" href="/" color="inherit" underline="none" variant="subtitle2" >
-                                Terms of Use ᛫
-                            </Link>
-                            <Link className="FooterLink" href="/" color="inherit" underline="none" variant="subtitle2" >
-                                Privacy Policy
-                            </Link>
-                            </Typography>
-                        </Box>
-                        <Box>
-                            <Typography className='LowestFooterBox'variant='subtitle2' align='center'>
-                                &copy; {new Date().getFullYear()} Cinetastisch Entertainment
-                            </Typography>
-                        </Box>
-                    </Grid>
-                    <Grid item xs={4} sm={4}>
-                    </Grid>
-                </Grid>
-            </Container>
-        </Box>
+export default function Footer() {
+  return (
+    <div>
+      <Box className="Footer-Box">
+        <Container className="footerContainer" sx={{ width: "100%" }}>
+          <Grid
+            container
+            spacing={0}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item xs={4} sm={4}></Grid>
+            <Grid item xs={4} sm={4}>
+              <Box>
+                <Typography align="center">
+                  <InfoIcon className="Footer-Icons"></InfoIcon>
+                  <AssistantDirectionIcon className="Footer-Icons"></AssistantDirectionIcon>
+                  <AccountCircleIcon className="Footer-Icons"></AccountCircleIcon>
+                </Typography>
+                <Typography align="center">
+                  <Link
+                    className="Footer-Link"
+                    href="/"
+                    color="inherit"
+                    underline="none"
+                    variant="subtitle2"
+                  >
+                    Info ᛫
+                  </Link>
+                  <Link
+                    className="Footer-Link"
+                    href="/"
+                    color="inherit"
+                    underline="none"
+                    variant="subtitle2"
+                  >
+                    Support ᛫
+                  </Link>
+                  <Link
+                    className="Footer-Link"
+                    href="/"
+                    color="inherit"
+                    underline="none"
+                    variant="subtitle2"
+                  >
+                    Marketing
+                  </Link>
+                </Typography>
+              </Box>
+
+              <Box>
+                <Typography align="center">
+                  <Link
+                    className="Footer-Link"
+                    href="/"
+                    color="inherit"
+                    underline="none"
+                    variant="subtitle2"
+                  >
+                    Terms of Use ᛫
+                  </Link>
+                  <Link
+                    className="Footer-Link"
+                    href="/"
+                    color="inherit"
+                    underline="none"
+                    variant="subtitle2"
+                  >
+                    Privacy Policy
+                  </Link>
+                </Typography>
+              </Box>
+              <Box>
+                <Typography
+                  className="Lowest-Footer-Box"
+                  variant="subtitle2"
+                  align="center"
+                >
+                  &copy; {new Date().getFullYear()} Cinetastisch Entertainment
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={4} sm={4}></Grid>
+          </Grid>
+        </Container>
+      </Box>
     </div>
+  );
 }
