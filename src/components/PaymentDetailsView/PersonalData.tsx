da9d04b275200d00c3e9fb316ddf429da4e72ae6
I549511 <johanna.deike@sap.com>
2022-12-01 16:08:14 +0100

    feat: tabs setup

--- src/components/PaymentDetailsView/PersonalData.tsx
+++ src/components/PaymentDetailsView/PersonalData.tsx
@@ -1,8 +1,14 @@
 import { VisibilityOff, Visibility } from "@mui/icons-material";
-import { Box, TextField, Button, Typography, FormControl, IconButton, InputAdornment, InputLabel, Link, OutlinedInput, ButtonGroup } from "@mui/material";
+import { Box, TextField, Button, Typography, FormControl, IconButton, InputAdornment, InputLabel, Link, OutlinedInput, ButtonGroup, AppBar, Tab, Tabs, useTheme } from "@mui/material";
 import React, { useState } from "react"
 import Login from "../Login";
 
+interface TabPanelProps {
+    children?: React.ReactNode;
+    dir?: string;
+    index: number;
+    value: number;
+}
 interface State {
     password: string;
     showPassword: boolean;
@@ -12,28 +18,48 @@ interface State {
     showRepeatedSignUpPassword: boolean;
 }
 
+function TabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
 
-function PersonalData() {
-    const [personalDataMode, setPersonalDataMode] = useState("guest");
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`personal-data-tabpanel-${index}`}
+            aria-labelledby={`personal-data-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+function a11yProps(index: number) {
+    return {
+        id: `personal-data-tab-${index}`,
+        'aria-controls': `personal-data-tabpanel-${index}`,
+    };
+}
+
+
+
+export default function FullWidthTabs() {
+    const theme = useTheme();
+    const [value, setValue] = React.useState(0);
+
+    const handleChangeTabs = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
 
     const guestMode = "guest";
     const signInMode = "signIn";
     const signUpMode = "signUp";
 
-    const handleSingnIn = (event: React.MouseEvent<HTMLElement>) => {
-        setPersonalDataMode(signInMode);
-    };
-
-    const handleSingnUp = (event: React.MouseEvent<HTMLElement>) => {
-        setPersonalDataMode(signUpMode);
-    };
-
-    const handleGuest = (event: React.MouseEvent<HTMLElement>) => {
-        setPersonalDataMode(guestMode);
-    };
-
     //for Login
-
     const [values, setValues] = React.useState<State>({
         password: '',
         showPassword: false,
@@ -76,20 +102,57 @@ function PersonalData() {
             setValues({ ...values, [prop]: event.target.value });
         };
 
-    //for Login End
-
-
-
-    if (personalDataMode === signInMode) {
-        return (
+    return (
+        <Box sx={{ bgcolor: 'background.paper', width: 500 }}>
+            <AppBar position="static">
+                <Tabs
+                    value={value}
+                    onChange={handleChangeTabs}
+                    indicatorColor="secondary"
+                    textColor="inherit"
+                    variant="fullWidth"
+                    aria-label="full width tabs example"
+                >
+                    <Tab label="Without Account" {...a11yProps(0)} />
+                    <Tab label="Sign In" {...a11yProps(1)} />
+                    <Tab label="Sign Up" {...a11yProps(2)} />
+                </Tabs>
+            </AppBar>
+            <TabPanel value={value} index={0} dir={theme.direction}>
             <div className="Login-Form-Content">
-                <Typography variant="h3" align="left" sx={{ pt: '3rem', pb: '2rem' }}>Sign In</Typography>
-                <Typography align="left">
-                    <ButtonGroup variant="outlined" sx={{ mb: '1rem' }}>
-                        <Button onClick={handleGuest}>Sign In as Guest User</Button>
-                        <Button onClick={handleSingnUp}>Create new account?</Button>
-                    </ButtonGroup>
-                </Typography>
+            <Box
+                component="form"
+                sx={{
+                    '& .MuiTextField-root': { m: 0.5, width: '100%' },
+                }}
+                noValidate
+                autoComplete="off"
+            >
+                <TextField
+                    required
+                    type="text"
+                    placeholder="Jane"
+                    label="First Name"
+                />
+
+                <TextField
+                    required
+                    type="text"
+                    placeholder="Doe"
+                    label="Surname"
+                />
+
+                <TextField
+                    required
+                    type="email"
+                    placeholder="Jane.doe@example.com"
+                    label="Email Address"
+                />
+            </Box>
+        </div>
+            </TabPanel>
+            <TabPanel value={value} index={1} dir={theme.direction}>
+            <div className="Login-Form-Content">
                 <Box
                     component="form"
                     sx={{
@@ -135,19 +198,9 @@ function PersonalData() {
                     </Button>
                 </Box>
             </div>
-        )
-    }
-
-    if (personalDataMode === signUpMode) {
-        return (
+            </TabPanel>
+            <TabPanel value={value} index={2} dir={theme.direction}>
             <div className="Login-Form-Content">
-                <Typography variant="h3" align="left" sx={{ pt: '3rem', pb: '2rem' }}>Sign Up</Typography>
-                <Typography align="left">
-                    <ButtonGroup variant="outlined" sx={{ mb: '1rem' }}>
-                        <Button onClick={handleGuest}>Sign In as Guest User</Button>
-                        <Button onClick={handleSingnIn}>Already registered?</Button>
-                    </ButtonGroup>
-                </Typography>
                 <Box
                     component="form"
                     sx={{
@@ -229,50 +282,36 @@ function PersonalData() {
                     <Button sx={{ m: 0.5, width: '100%' }} variant="contained">Sign Up</Button>
                 </div>
             </div>
+            </TabPanel>
+        </Box>
+    );
+}
+
+
+/* 
+
+
+
+function PersonalData() {
+    
+
+
+    if (personalDataMode === signInMode) {
+        return (
+            
+        )
+    }
+
+    if (personalDataMode === signUpMode) {
+        return (
+           
         )
     }
 
     return (
-        <div className="Login-Form-Content">
-            <Typography variant="h3" sx={{ pt: '3rem', pb: '2rem' }}>Personal Data</Typography>
-            <Typography align="left">
-                <ButtonGroup variant="outlined" sx={{ mb: '1rem' }}>
-                    <Button onClick={handleSingnIn}>Already registered?</Button>
-                    <Button onClick={handleSingnUp}>Create new account?</Button>
-                </ButtonGroup>
-            </Typography>
-            <Box
-                component="form"
-                sx={{
-                    '& .MuiTextField-root': { m: 0.5, width: '100%' },
-                }}
-                noValidate
-                autoComplete="off"
-            >
-                <TextField
-                    required
-                    type="text"
-                    placeholder="Jane"
-                    label="First Name"
-                />
-
-                <TextField
-                    required
-                    type="text"
-                    placeholder="Doe"
-                    label="Surname"
-                />
-
-                <TextField
-                    required
-                    type="email"
-                    placeholder="Jane.doe@example.com"
-                    label="Email Address"
-                />
-            </Box>
-        </div>
+        
     )
 
 }
 
-export default PersonalData;
+export default PersonalData; */
